92a8c7aae22e34b96386cce1f6947d8a5e902546
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2018-06-25 10:56:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<!-- Designed By Parveen Kumar (www.parveenbhadoo.com) -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Café Coffee Day - Home</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<marquee onmouseover="stop()" onmouseout="start()" behavior="alternate" >The Project is not Complete Yet, Work in Progress...</marquee>
+		<div>
+			<div id="logo">
+				<a href="index.php"><img src="images/logo.png" alt="Café Coffee Day"></a>
+			</div>
+			<ul id="navigation">
+				<li class="selected">
+					<a href="index.php">Home</a>
+				</li>
+				<li>
+					<a href="about.php">About</a>
+				</li>
+				<li>
+					<a href="gallery.php">Gallery</a>
+				</li>
+				<li>
+					<a href="blog.php">Blog</a>
+				</li>
+				<li>
+					<a href="contact.php">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div id="contents">
+		<div>
+			<div class="body">
+				<ul id="featured">
+					<li>
+						<div>
+							<img src="images/slider.jpg" alt="Slider Here" > <a href="#"></a>
+						</div>
+					</li>
+				</ul>
+				<div id="adbox">
+					<img src="images/cake.gif" alt="Black Forrest Cake"> <span class="info"> Black Forrest Cake <em>Rs.325</em> <a href="index.php" class="btn1">View Details</a> </span>
+					<div class="details">
+						<h1>Magical<br> Celebrations<br> #MakeItMagical</h1>
+						<p>
+							Café Coffee Day, a part of Coffee Day Global Limited, is India’s favourite hangout for coffee and conversations. Popularly known as CCD, we strive to provide the best experience to our guests. Our coffees are sourced from thousands of small coffee planters, who made us who we are today and we're glad to be a part of their lives.
+						</p>
+					</div>
+				</div>
+				<ul id="featured">
+					<li>
+						<div>
+							<img src="images/cover.jpg" alt="Celebrations" > <a href="#"></a>
+						</div>
+					</li>
+				</ul>
+				<div class="contact">
+					<p>
+						For Order and Inquiries Please Call: <b>1800 102 5093</b> Or you can contact us at: <p>customercare@cafecoffeeday.com </p>
+					</p>
+				</div>
+				<div class="blog">
+					<div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div>
+			<div id="links">
+				<div class="showroom">
+					<h4>Visit our Showroom</h4>
+					<a href="gallery.php"><img src="images/show-room.png" alt="Img"></a>
+					<p>
+						4885 Wilson Street<br> Victorville, CA 92392<br><br> 760-962-9541<br><br> <a href="index.html">info@carvedcreations.com</a>
+					</p>
+				</div>
+				<div>
+					<h4>Recent Blog Posts</h4>
+					<ul class="posts">
+						<li>
+							<span class="time">Apr 16</span>
+							<p>
+								<a href="blog.php">The Carving Master &amp; Owner</a> Maybe you’re looking for something diferent, something special.
+							</p>
+						</li>
+						<li>
+							<span class="time">Apr 15</span>
+							<p>
+								<a href="blog.php">5 Star Hotels We Supply</a> And we love the challenge of doing something diferent and something special.
+							</p>
+						</li>
+						<li>
+							<span class="time">Apr 14</span>
+							<p>
+								<a href="blog.php">How To Pick The Right Furniture For You</a> What’s more, they’re absolutely free! You can do a lot with them.
+							</p>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<form action="#" method="post" id="newsletter">
+						<h4>Join Our Newsletter</h4>
+						<input type="text" value="Enter Email Address Here For Updates" onBlur="javascript:if(this.value==''){this.value=this.defaultValue;}" onFocus="javascript:if(this.value==this.defaultValue){this.value='';}">
+						<input type="submit" value="Sign up" class="btn2" />
+					</form>
+					<div id="connect">
+						<h4>Social Media</h4>
+						<a href="https://www.google.co.in/search?q=facebook.com%2Fparveenbhadoo" target="_blank" class="facebook"></a> <a href="https://www.google.co.in/search?q=parveen+bhadoo+google+plus" target="_blank" class="googleplus"></a> <a href="https://www.google.co.in/search?q=twitter.com%2Fparveenbhadoo" target="_blank" class="twitter"></a>
+					</div>
+				</div>
+			</div>
+			<ul class="navigation">
+				<li>
+					<a href="index.php">Home</a>
+				</li>
+				<li>
+					<a href="about.php">About</a>
+				</li>
+				<li>
+					<a href="gallery.php">Gallery</a>
+				</li>
+				<li>
+					<a href="blog.php">Blog</a>
+				</li>
+				<li>
+					<a href="contact.php">Contact</a>
+				</li>
+			</ul>
+			<p id="footnote">
+				© Copyright 2018. Parveen Kumar, All Rights Reserved.
+			</p>
+		</div>
+	</div>
+</body>
+</html>
